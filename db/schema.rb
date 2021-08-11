c50833db9fdb6913fb562d4ae5d55f0137b041aa
Benedikt Nolte <bene_nolte@me.com>
2021-08-11 17:06:24 +0200

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_134316) do
+ActiveRecord::Schema.define(version: 2021_08_11_134824) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -18,14 +18,6 @@ ActiveRecord::Schema.define(version: 2021_08_11_134316) do
     t.text "description"
     t.integer "price_per_night"
     t.integer "number_of_guests"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "stars"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  post = Flat.new(
+    name: Faker::Beer.brand,
+    address: Faker::Address.street_address,
+    description: Faker::Twitter.status[:text],
+    price_per_night: rand(500),
+    number_of_guests: rand(10)
+  )
+  post.save!
+end
